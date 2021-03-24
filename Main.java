74b23d46c5a74b3a727b0cd6082f50930a7eac9d
IonutTache <81294614+IonutTache@users.noreply.github.com>
2021-03-24 13:26:30 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,20 +3,20 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Dimensions dimensions = new Dimensions(20, 20, 5);
-        Case theCase = new Case("220B", "Dell", "240", dimensions);
+//         Dimensions dimensions = new Dimensions(20, 20, 5);
+//         Case theCase = new Case("220B", "Dell", "240", dimensions);
 
 
-        Monitor theMonitor = new Monitor("27inch Beast", "Acer", 27, new Resolution(2540, 1440));
+//         Monitor theMonitor = new Monitor("27inch Beast", "Acer", 27, new Resolution(2540, 1440));
 
-        MotherBoard theMotherBoard = new MotherBoard("BJ-200", "Asus", 4, 6, "v2.44");
+//         MotherBoard theMotherBoard = new MotherBoard("BJ-200", "Asus", 4, 6, "v2.44");
 
-        PC thePC = new PC(theCase, theMonitor, theMotherBoard);
+//         PC thePC = new PC(theCase, theMonitor, theMotherBoard);
 //        thePC.getMonitor().drawPixelAt(1500,1200,"red"); /////////// Tim le-a sters ptr ca sters si geterururile din PC class
 //        thePC.getMotherBoard().loadProgram("Windows 1.0");
 //        thePC.getTheCase().pressPowerButton();
 
-        thePC.powerUp();
+//         thePC.powerUp();
 
 
         Wall wall1 = new Wall("WEST");
